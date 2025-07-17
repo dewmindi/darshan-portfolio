1c5cfc63ca663dcd00ce51c23451931f30d32506
dewmindi <udewmindi.ud@gmail.com>
2025-07-17 14:28:45 +0530

    added google indexing file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { fellFrench } from './fonts/fell-french';
 export const metadata: Metadata = {
   title: 'Lawyer Darshan',
   description: 'Migration Lawyer Australia',
+    icons: {
+    icon: '/favicon.png', // or .png, .svg depending on what you have
+  }
 }
 
 export default function RootLayout({
